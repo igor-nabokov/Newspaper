4f6d5cfdcc546ab58ba8acfc93a45a4a1ab86b00
igor-nabokov <baldur.hermod@gmail.com>
2017-11-18 18:30:48 +0200

    FixedSlider

--- script.js
+++ script.js
@@ -7,28 +7,25 @@ function burgerMenu () {
 	}
 }
 
-document.addEventListener (
-	"DOMContentloaded", function () {
-		var imgArr = ['images/1.jpg', 'images/2.jpg', 'images/3.jpg'];
-		console.log('1111');
-		var slid = document.getElementById("image");
-		slid.src = imgArr[0];
-		var right = document.getElementById("rigthbutton");
+window.addEventListener("DOMContentLoaded", function () {
+	var imgArr = ['images/1.jpg', 'images/2.jpg', 'images/3.jpg'];
+	var slid = document.getElementById("image");
+	slid.src = imgArr[0];
+	var right = document.getElementById("rightbutton");
 		
-		var left = document.getElementById("leftbutton");
-		right.addEventListener('click', next, false);
+	var left = document.getElementById("leftbutton");
+	right.addEventListener('click', next, false);
 		
-		left.addEventListener('click', prev, false);
-		i=0;
-		function next () {
-			if (i==2) {i=0} else {i++}
-			slid.src = imgArr[i];
-		}
-	
-		function prev () {
-			if (i == 0) {i=2}
-			else {i--}
-			slid.src = imgArr[i];
-		}
+	left.addEventListener('click', prev, false);
+	i=0;
+	function next () {
+		if (i==2) {i=0} else {i++}
+		slid.src = imgArr[i];
 	}
-	)
+	
+	function prev () {
+		if (i == 0) {i=2}
+		else {i--}
+		slid.src = imgArr[i];
+	}
+});
